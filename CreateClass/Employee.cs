760df7181fca4f5a586cd617689275763398e55c
Judit <tolnai.judit12@gmail.com>
2017-06-04 00:41:31 +0200

    Employee, Person classes

---
+++ CreateClass/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreateClass
+{
+    class Employee : Person
+    {
+        private String salary;
+        private String profession;
+
+        public Employee(string name, int dateBirth, String salary, String profession) : base(name, dateBirth)
+        {
+            this.salary = salary;
+            this.profession = profession;
+        }
+
+
+    }
+}
---
+++ CreateClass/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreateClass
+{
+    class Person
+    {
+        private String name;
+        private int dateBirth;
+
+        public Person(String name, int dateBirth)
+        {
+            this.name = name;
+            this.dateBirth = dateBirth;
+        }
+
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public int getDateBirth()
+        {
+            return this.dateBirth;
+        }
+
+        enum Gender { Female, Male }
+
+        public override string ToString()
+        {
+            return String.Format("This instance of my object has the following: Name = {0}, BirthDate = {1}", name, dateBirth);
+        }
+    }
+}
